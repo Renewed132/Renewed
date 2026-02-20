8a0501f08448a221cc5ca0d441ec2da431086c5f
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-02-20 13:13:10 +0100

    Initial commit

---
+++ src/main/java/pl/olafcio/renewed/Renewed.java
@@ -0,0 +1,10 @@
+package pl.olafcio.renewed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Renewed implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        //
+    }
+}
