9fb6c732947f6908a579368b4a713e861e532397
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-02-21 12:18:17 +0100

    Own translations

---
+++ src/main/java/pl/olafcio/renewed/mixin/LanguageMixin.java
@@ -0,0 +1,44 @@
+package pl.olafcio.renewed.mixin;
+
+import net.minecraft.util.Language;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pl.olafcio.renewed.Renewed;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+@Mixin(Language.class)
+public class LanguageMixin {
+    @Inject(at = @At("TAIL"), method = "method_633")
+    private void loadLanguage(Properties properties, String language, CallbackInfo ci) {
+        try {
+            InputStream resource = Renewed.class.getResourceAsStream("/assets/renewed/lang/" + language + ".lang");
+            if (resource == null)
+                return;
+
+            BufferedReader var3 = new BufferedReader(
+                                  new InputStreamReader(
+                                        resource,
+                                        "UTF-8"
+                                  ));
+
+            for (String var4 = var3.readLine(); var4 != null; var4 = var3.readLine()) {
+                var4 = var4.trim();
+                if (!var4.startsWith("#")) {
+                    String[] var5 = var4.split("=");
+                    if (var5 != null && var5.length == 2) {
+                        properties.setProperty(var5[0], var5[1]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
