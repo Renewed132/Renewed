8aa04c2c88d2ac0075acbed2b541eef72b9467dd
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-02-26 23:06:01 +0100

    [1.4.2] Make spawn eggs spawn sheep with random colors

--- src/main/java/pl/olafcio/renewed/mixin/SpawnEggItemMixin.java
+++ src/main/java/pl/olafcio/renewed/mixin/SpawnEggItemMixin.java
@@ -1,16 +1,24 @@
 package pl.olafcio.renewed.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.CommonI18n;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
 
 @Mixin(SpawnEggItem.class)
 public class SpawnEggItemMixin extends Item {
@@ -28,5 +36,17 @@ public class SpawnEggItemMixin extends Item {
             var2 = CommonI18n.translate("entity." + var3 + ".name") + " " + var2;
 
         cir.setReturnValue(var2);
+    }
+
+    @Unique
+    private static final Random random
+            = new Random();
+
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z", ordinal = 1), method = "method_3456")
+    private static boolean spawnEntity(World instance, Entity entity, Operation<Boolean> original) {
+        if (entity instanceof SheepEntity)
+            ((SheepEntity) entity).method_2862(random.nextInt(15));
+
+        return original.call(instance, entity);
     }
 }
