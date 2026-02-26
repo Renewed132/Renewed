45428db016737b56386bef8b77fa3557e77d2b0e
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-02-26 23:22:25 +0100

    /teleport alias for /tp

---
+++ src/main/java/pl/olafcio/renewed/mixin/TeleportCommandMixin.java
@@ -0,0 +1,18 @@
+package pl.olafcio.renewed.mixin;
+
+import net.minecraft.command.AbstractCommand;
+import net.minecraft.server.command.TeleportCommand;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.Collections;
+import java.util.List;
+
+@Mixin(TeleportCommand.class)
+public abstract class TeleportCommandMixin
+       extends AbstractCommand
+{
+    @Override
+    public List<String> getAliases() {
+        return Collections.singletonList("teleport");
+    }
+}
