6e3f4a8500dfff991683265bb60aff2ad5810c34
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-28 19:05:08 +0100

    (0.3-beta) [Commands] [/teleport] Set minimum Y to -64

--- src/main/java/pl/olafcio/renewed/mixin/commands/aliases/TeleportCommandMixin.java
+++ src/main/java/pl/olafcio/renewed/mixin/commands/aliases/TeleportCommandMixin.java
@@ -3,6 +3,8 @@ package pl.olafcio.renewed.mixin.commands.aliases;
 import net.minecraft.command.AbstractCommand;
 import net.minecraft.server.command.TeleportCommand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,5 +16,12 @@ public abstract class TeleportCommandMixin
     @Override
     public List<String> getAliases() {
         return Collections.singletonList("teleport");
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(ordinal = 1, intValue = 0)
+    }, method = "execute")
+    public int execute__minimumY(int constant) {
+        return -64;
     }
 }
