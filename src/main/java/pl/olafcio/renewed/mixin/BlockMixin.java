d00281be6dbb12a237c05acf2c0b6a6a24a5d191
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-31 00:09:43 +0200

    (0.7-beta) Make a magic (auto) -1 block ID

--- src/main/java/pl/olafcio/renewed/mixin/BlockMixin.java
+++ src/main/java/pl/olafcio/renewed/mixin/BlockMixin.java
@@ -1,10 +1,12 @@
 package pl.olafcio.renewed.mixin;
 
 import net.minecraft.block.Block;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pl.olafcio.renewed.features.NewBlock;
 import pl.olafcio.renewed.mixininterface.Translatable;
@@ -17,6 +19,21 @@ public abstract class BlockMixin
     private static void addCustomRegistries(CallbackInfo ci) {
         NewBlock.init();
     }
+
+    @ModifyVariable(at = @At("HEAD"), name = "i", index = 1, ordinal = 0, method = "<init>(ILnet/minecraft/block/material/Material;)V", argsOnly = true)
+    private static int init__blockID(int i) {
+        if (i == -1) {
+            for (int id = 1; id < BLOCKS.length; id++)
+                if (BLOCKS[id] == null)
+                    return id;
+        }
+
+        return i;
+    }
+
+    @Shadow
+    @Final
+    public static Block[] BLOCKS;
 
     @Shadow
     public abstract String getTranslationKey();
