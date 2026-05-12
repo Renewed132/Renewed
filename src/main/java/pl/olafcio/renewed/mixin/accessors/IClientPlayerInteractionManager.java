837698ab157ae491c51a4e21c13f0f4dd0a44f22
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-05-12 19:31:27 +0200

    (26.5/4) Accessor for IClientPlayerInteractionManager#gameMode

---
+++ src/main/java/pl/olafcio/renewed/mixin/accessors/IClientPlayerInteractionManager.java
@@ -0,0 +1,12 @@
+package pl.olafcio.renewed.mixin.accessors;
+
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.world.GameMode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public interface IClientPlayerInteractionManager {
+    @Accessor("gameMode")
+    GameMode gameMode();
+}
