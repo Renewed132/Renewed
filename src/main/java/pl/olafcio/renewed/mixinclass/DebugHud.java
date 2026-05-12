d5f09b7a1ca821e70f7d94fa598f743101f8105f
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-05-12 19:25:32 +0200

    (26.5/4) DebugHud refactor elements

--- src/main/java/pl/olafcio/renewed/mixinclass/DebugHud.java
+++ src/main/java/pl/olafcio/renewed/mixinclass/DebugHud.java
@@ -28,33 +28,7 @@ public final class DebugHud {
                 ? this.mc.world.getChunkFromPos(x, z)
                 : null;
 
-        String[] left = new String[]{
-                "Minecraft {} ({}/vanilla)".replace("{}", FabricLoader.getInstance().getRawGameVersion()),
-                this.mc.fpsDebugString,
-                this.mc.getChunkDebugString(),
-                this.mc.getEntitiesDebugString(),
-                "T: " + this.mc.world.loadedEntities.size(),
-//                "Chunks[C] W: 0, 0 E: 0,0,0",
-//                "Chunks[S] W: 0, E: 0,0,0,0,0,0,0",
-                this.mc.world.dimension.getName(),
-                "",
-                String.format("XYZ: %.3f / %.5f / %.3f", this.mc.cameraEntity.x, this.mc.cameraEntity.boundingBox.minY, this.mc.cameraEntity.z),
-                String.format("Block: %.0f %.0f %.0f", this.mc.cameraEntity.x, this.mc.cameraEntity.y, this.mc.cameraEntity.z),
-                String.format("Chunk: %d %d %d [%.0f %.0f in r.%d.%d.mca]", chunk.chunkX, (int)this.mc.cameraEntity.y >> 4, chunk.chunkZ, this.mc.cameraEntity.x - chunk.chunkX*16, this.mc.cameraEntity.z - chunk.chunkZ*16, chunk.chunkX, chunk.chunkZ),
-//                "Facing: ",
-                String.format("Client Light: %d (%d sky, %d block)", this.mc.world.getLightmapCoordinates(x, y, z, 0), this.mc.world.method_3642(LightType.SKY, x, y, z), this.mc.world.method_3642(LightType.BLOCK, x, y, z)),
-//                "CH S: 0 M: 0",
-//                "SH S: 0 O: 0 M: 0 ML: 0",
-                "Biome: " + chunk.getBiome(x & 0xF, z & 0xF, this.mc.world.getBiomeSource()).name,
-                String.format("Local Difficulty: %d", this.mc.world.difficulty),
-//                "NoiseRouter ...",
-//                "Biome builder PV: 0 C: Far inland E: 0 T: 0 H: 1",
-//                "SC: 0, M: 0, C: 0, A: 0, A: 0, U: 0, W: 0, W: 0, M: 0",
-//                String.format("Sounds: %d/247", this.mc.soundSystem.),
-                "",
-                "Debug: Pie [shift]: " + (this.mc.options.debugProfilerEnabled?"visible":"invisible"),
-                "For help: press F3 + Q"
-        };
+        String[] left = getLeft(chunk, x, y, z);
 
         int textY = 2;
         for (String line : left) {
@@ -69,17 +43,7 @@ public final class DebugHud {
         long memFree = Runtime.getRuntime().freeMemory();
         long memUsed = memTotal - memFree;
 
-        String[] right = new String[]{
-                "Java: " + System.getProperty("java.version") + " " + System.getProperty("sun.arch.data.model") + "bit",
-                "Mem:  " + memUsed * 100L / memMax + "% " + memUsed / 1024L / 1024L + "/" + memMax / 1024L / 1024L + "MB",
-                "Allocated:  " + memTotal * 100L / memMax + "% " + memTotal / 1024L / 1024L + "MB",
-                "",
-//                "CPU: ProcessorName",
-//                "",
-                "Display: " + mc.width + "x" + mc.height + " (Brand)"
-//                "GraphicsCard",
-//                "0.0.0 - Build 0.0.0.0"
-        };
+        String[] right = getRight(memUsed, memMax, memTotal);
 
         textY = 2;
         for (String line : right) {
@@ -96,6 +60,50 @@ public final class DebugHud {
 //        if (this.mc.world != null && this.mc.world.isPosLoaded(n3, n2, n)) {
 //            Chunk chunk = this.mc.world.getChunkFromPos(n3, n);
 //        }
+    }
+
+    private String[] getRight(long memUsed, long memMax, long memTotal) {
+        return new String[]{
+                "Java: " + System.getProperty("java.version") + " " + System.getProperty("sun.arch.data.model") + "bit",
+                "Mem:  " + memUsed * 100L / memMax + "% " + memUsed / 1024L / 1024L + "/" + memMax / 1024L / 1024L + "MB",
+                "Allocated:  " + memTotal * 100L / memMax + "% " + memTotal / 1024L / 1024L + "MB",
+                "",
+//                "CPU: ProcessorName",
+//                "",
+                "Display: " + mc.width + "x" + mc.height + " (Brand)"
+//                "GraphicsCard",
+//                "0.0.0 - Build 0.0.0.0"
+        };
+    }
+
+    private String[] getLeft(Chunk chunk, int x, int y, int z) {
+        return new String[]{
+                "Minecraft {} ({}/vanilla)".replace("{}", FabricLoader.getInstance().getRawGameVersion()),
+                this.mc.fpsDebugString,
+                this.mc.getChunkDebugString(),
+                this.mc.getEntitiesDebugString(),
+                "T: " + this.mc.world.loadedEntities.size(),
+//                "Chunks[C] W: 0, 0 E: 0,0,0",
+//                "Chunks[S] W: 0, E: 0,0,0,0,0,0,0",
+                this.mc.world.dimension.getName(),
+                "",
+                String.format("XYZ: %.3f / %.5f / %.3f", this.mc.cameraEntity.x, this.mc.cameraEntity.boundingBox.minY, this.mc.cameraEntity.z),
+                String.format("Block: %.0f %.0f %.0f", this.mc.cameraEntity.x, this.mc.cameraEntity.y, this.mc.cameraEntity.z),
+                String.format("Chunk: %d %d %d [%.0f %.0f in r.%d.%d.mca]", chunk.chunkX, (int) this.mc.cameraEntity.y >> 4, chunk.chunkZ, this.mc.cameraEntity.x - chunk.chunkX * 16, this.mc.cameraEntity.z - chunk.chunkZ * 16, chunk.chunkX, chunk.chunkZ),
+//                "Facing: ",
+                String.format("Client Light: %d (%d sky, %d block)", this.mc.world.getLightmapCoordinates(x, y, z, 0), this.mc.world.method_3642(LightType.SKY, x, y, z), this.mc.world.method_3642(LightType.BLOCK, x, y, z)),
+//                "CH S: 0 M: 0",
+//                "SH S: 0 O: 0 M: 0 ML: 0",
+                "Biome: " + chunk.getBiome(x & 0xF, z & 0xF, this.mc.world.getBiomeSource()).name,
+                String.format("Local Difficulty: %d", this.mc.world.difficulty),
+//                "NoiseRouter ...",
+//                "Biome builder PV: 0 C: Far inland E: 0 T: 0 H: 1",
+//                "SC: 0, M: 0, C: 0, A: 0, A: 0, U: 0, W: 0, W: 0, M: 0",
+//                String.format("Sounds: %d/247", this.mc.soundSystem.),
+                "",
+                "Debug: Pie [shift]: " + (this.mc.options.debugProfilerEnabled ? "visible" : "invisible"),
+                "For help: press F3 + Q"
+        };
     }
 
     private void draw(String text, int x, int y) {
