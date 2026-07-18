96c32f5d523c263e1403e99d412faa43acb0515b
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-18 04:33:04 +0200

    (26.7/1) Spectator language definition

--- src/main/java/pl/olafcio/renewed/mixin/GameModeCommandMixin.java
+++ src/main/java/pl/olafcio/renewed/mixin/GameModeCommandMixin.java
@@ -17,12 +17,12 @@ import pl.olafcio.renewed.mixininterface.IServerPlayerEntity;
 @Mixin(GameModeCommand.class)
 public class GameModeCommandMixin {
     @Unique
-    private boolean spectator = false;
+    private int spectator = 0;
 
     @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/GameModeCommand;method_3540(Lnet/minecraft/command/CommandSource;Ljava/lang/String;)Lnet/minecraft/world/GameMode;"), method = "execute")
     public GameMode execute__parseGamemode(GameModeCommand instance, CommandSource source, String string, Operation<GameMode> original) {
         if (string.equalsIgnoreCase("spectator") || string.equalsIgnoreCase("sp")) {
-            this.spectator = true;
+            this.spectator = 1;
             return GameMode.ADVENTURE;
         }
 
@@ -31,8 +31,8 @@ public class GameModeCommandMixin {
 
     @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;method_3170(Lnet/minecraft/world/GameMode;)V"), method = "execute")
     public void execute__setGamemode(PlayerEntity instance, GameMode gameMode, Operation<Void> original) {
-        if (this.spectator) {
-            this.spectator = false;
+        if (this.spectator == 1) {
+            this.spectator = 2;
             instance.noClip = true;
 
             ServerPlayerEntity player = (ServerPlayerEntity) instance;
@@ -60,13 +60,23 @@ public class GameModeCommandMixin {
         }
     }
 
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameMode;getGameModeName()Ljava/lang/String;"), method = "execute")
+    public String execute__getGamemodeName(GameMode instance, Operation<String> original) {
+        if (this.spectator == 2) {
+            this.spectator = 0;
+            return "spectator";
+        }
+
+        return original.call(instance);
+    }
+
     @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/GameModeCommand;getClampedInt(Lnet/minecraft/command/CommandSource;Ljava/lang/String;II)I"), method = "method_3540")
     protected int parseGamemode__getClampedInt(CommandSource commandSource, String string, int min, int max, Operation<Integer> original) {
         int spectator = max + 1;
         int val = original.call(commandSource, string, min, spectator);
 
         if (val == spectator) {
-            this.spectator = true;
+            this.spectator = 1;
             return GameMode.ADVENTURE.getGameModeId();
         }
 
