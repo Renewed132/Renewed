1c5f1b32972482ab39d49d359a51597abc9944a0
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-20 21:58:00 +0200

    (26.7/1) Extend modern block handling

--- src/main/java/pl/olafcio/renewed/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/pl/olafcio/renewed/mixin/ServerPlayerInteractionManagerMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.world.World;
@@ -14,7 +15,7 @@ public class ServerPlayerInteractionManagerMixin {
     @WrapMethod(method = "method_2170")
     public boolean useBlock(PlayerEntity player, World world, ItemStack stack, int x, int y, int z, int i, float f, float g, float h, Operation<Boolean> original) {
         int block = world.getBlock(x, y, z);
-        if (block == Block.SNOW_LAYER.id && !player.isSneaking())
+        if ((block == Block.SNOW_LAYER.id || block == Block.TRIPWIRE.id || block == Block.TALLGRASS.id) && !player.isSneaking())
             y -= 1;
 
         return original.call(player, world, stack, x, y, z, i, f, g, h);
